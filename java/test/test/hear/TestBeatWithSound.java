90afb0f3a10e5d008c55abc3566447cce59bf039
zaboople <troymotte@yahoo.com>
2018-09-24 18:13:41 -0500

    Better TestBeatWithSound

--- java/test/test/hear/TestBeatWithSound.java
+++ java/test/test/hear/TestBeatWithSound.java
@@ -2,29 +2,21 @@ package test.hear;
 import org.tmotte.pm.MyMidi3;
 import org.tmotte.pm.Player;
 
-/** Look in java System properties for sound values. */
+/** This should play 2 rolls per second. */
 public class TestBeatWithSound implements XTest {
     public static void main(String args[]) throws Exception {
 	    new TestBeatWithSound().test(new MyMidi3(), true);
     }
     public @Override void test(MyMidi3 midi, boolean stop)  {
 	    Player player=new Player()
-		    .setBeatsPerMinute(60)
+		    .setBeatsPerMinute(120)
 		    .octave(2);
-		int checks=10;
+		int checks=20;
 		for (int i=0; i<checks; i++)
-			if (i%2==0)
-			    player.c(4, 24+i)
-				    .r(16).n(8, 28+i).up()
-				    .r(8).n(8, 31+i).up()
-				    .r(8.).n(16, 31+i).up().up();
-		    else
-			    player.c(4, 24+i)
-				    .r(16).n(8., 28+i).up()
-				    .r(8).n(16.3, 31+i).up()
-				    .r(8).t(16.3).n(16.3, 31+i).up()
-				    .r(8).t(16.3).t(16.3).n(16.3, 33+i).up()
-				    .up();
+		    player.c(4, 24+i)
+			    .r(16).n(8, 28+i).up()
+			    .r(8).n(8, 31+i).up()
+			    .r(8.).n(16, 31+i).up().up();
 	    midi.sequence(player);
 	    doTimerThread(checks);
 	    midi.play(stop);
@@ -33,7 +25,7 @@ public class TestBeatWithSound implements XTest {
 	    new Thread() {
 		    public void run() {
 			    long before=System.currentTimeMillis();
-			    for (int i=0; i<10+1; i++) {
+			    for (int i=0; i<10+2; i++) {
 				    for (int j=0; j<10; j++) {
 					    try {
 						    Thread.sleep(100);
