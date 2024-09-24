d6c4b15e83dc12a42cc20ffdaa0af61441a1b19d
zaboople <troy.motte@gmail.com>
2024-09-24 12:47:18 -0500

    MidiTracker - tabs vs indent problems

--- java/main/org/tmotte/common/midi/MidiTracker.java
+++ java/main/org/tmotte/common/midi/MidiTracker.java
@@ -19,8 +19,6 @@ import javax.sound.midi.*;
  * A general-purpose class for sending Midi Messages to a given Track. Essentially this provides a lot of sequencing
  * methods that the Java Midi API should already have, but "forgot" to include. A lot of these were quite difficult to
  * figure out.
- *
- * FIXME all these "send" things are silly idioms.
  */
 public class MidiTracker  {
 
@@ -29,36 +27,36 @@ public class MidiTracker  {
     public MidiTracker() {}
 
     public MidiTracker(Track t) {
-	    this.currTrack=t;
+        this.currTrack=t;
     }
 
     public MidiTracker createTrack(Sequence sequence) {
-	    return setTrack(sequence.createTrack());
+        return setTrack(sequence.createTrack());
     }
 
     public MidiTracker setTrack(Track t) {
-	    currTrack=t;
-	    return this;
+        currTrack=t;
+        return this;
     }
 
-	public void noteOn(int channel, int pitch, int volume, long tick) {
-		// NOTE_ON=144
-		Log.log("MidiTracker", "noteOn() tick {} pitch {} volume {}", tick, pitch, volume);
+    public void noteOn(int channel, int pitch, int volume, long tick) {
+        // NOTE_ON=144
+        Log.log("MidiTracker", "noteOn() tick {} pitch {} volume {}", tick, pitch, volume);
         event(channel, ShortMessage.NOTE_ON, pitch, volume, tick);
     }
 
-	public void noteOff(int channel, int pitch, long tick) {
+    public void noteOff(int channel, int pitch, long tick) {
         event(channel, ShortMessage.NOTE_OFF, pitch, 0, tick);
     }
 
 
-	public void sendInstrument(int channel, Instrument instr, long tick) {
+    public void sendInstrument(int channel, Instrument instr, long tick) {
         Patch patch=instr.getPatch();
-		int bank   =patch.getBank();
-		sendControlChange(channel, 0,  getMSB(bank), tick);
+        int bank   =patch.getBank();
+        sendControlChange(channel, 0,  getMSB(bank), tick);
         sendControlChange(channel, 32, getLSB(bank), tick);
         event(channel, ShortMessage.PROGRAM_CHANGE, patch.getProgram(), 0, tick);
-	}
+    }
 
 
     /** Short for Bend Sensitivity (hard word to type). */
@@ -74,7 +72,7 @@ public class MidiTracker  {
         sendControlChange(channel, 38, 0, tick);
     }
     public void sendBend(int channel, int amount, long tick) {
-		Log.log("MidiTracker", "Bend {} at {} ", amount, tick);
+        Log.log("MidiTracker", "Bend {} at {} ", amount, tick);
         // A bend is 14 bits - no, not 16. That won't fit in a byte, but
         // even better, you are required to split it into a 7-bits-each pair.
         event(channel, ShortMessage.PITCH_BEND, getLSB(amount), getMSB(amount), tick);
@@ -83,62 +81,62 @@ public class MidiTracker  {
         sendBend(channel, 8192, tick);
     }
 
-	/** This is vibrato (usually) */
+    /** This is vibrato (usually) */
     public void sendPressure(int channel, int amount, long tick) {
-	    event(channel, ShortMessage.CHANNEL_PRESSURE, amount, 0, tick);
+        event(channel, ShortMessage.CHANNEL_PRESSURE, amount, 0, tick);
     }
 
-	/** This is volume swells */
+    /** This is volume swells */
     public void sendExpression(int channel, int volume, long tick) {
-		Log.log("MidiTracker", "sendExpression() tick {} volume {}", tick, volume);
-	    sendControlChange(channel, 11, volume,  tick);
+        Log.log("MidiTracker", "sendExpression() tick {} volume {}", tick, volume);
+        sendControlChange(channel, 11, volume,  tick);
     }
 
 
 
-	/**
-	 * Portamento time appears to be limited to 0-127, but then we're allowed to
-	 * send a MSB and LSB and so I'm not sure. And it looks like the Java synthesizer
-	 * doesn't handle portamento at all, not to mention that there's ambiguity about
-	 * polyphony/chords to begin with.
-	 * @param amount 0-127 or 0-4095 (14 bits)
-	 */
+    /**
+     * Portamento time appears to be limited to 0-127, but then we're allowed to
+     * send a MSB and LSB and so I'm not sure. And it looks like the Java synthesizer
+     * doesn't handle portamento at all, not to mention that there's ambiguity about
+     * polyphony/chords to begin with.
+     * @param amount 0-127 or 0-4095 (14 bits)
+     */
     public void sendPortamentoTime(int channel, int amount, long tick) {
-		Log.log("MidiTracker", "sendPortamentoTime() tick {} amount {}", tick, amount);
-	    sendControlChange(channel, 5, getMSB(amount),  tick);
-	    sendControlChange(channel, 37, getLSB(amount),  tick);
+        Log.log("MidiTracker", "sendPortamentoTime() tick {} amount {}", tick, amount);
+        sendControlChange(channel, 5, getMSB(amount),  tick);
+        sendControlChange(channel, 37, getLSB(amount),  tick);
     }
-	/**
-	 * @param noteFrom The note to (sorta) "slur" from when the next note-on
-	 * message is received.
-	 */
+    /**
+     * @param noteFrom The note to (sorta) "slur" from when the next note-on
+     * message is received.
+     */
     public void sendPortamento(int channel, int noteFrom, long tick) {
-		Log.log("MidiTracker", "sendPortamento() tick {} noteFrom {}", tick, noteFrom);
-		// Portamento on, then send the note to "slur" from:
-	    sendControlChange(channel, 65, 64,  tick);
-	    sendControlChange(channel, 84, noteFrom,  tick);
+        Log.log("MidiTracker", "sendPortamento() tick {} noteFrom {}", tick, noteFrom);
+        // Portamento on, then send the note to "slur" from:
+        sendControlChange(channel, 65, 64,  tick);
+        sendControlChange(channel, 84, noteFrom,  tick);
     }
 
     public void sendPortamentoOff(int channel, long tick) {
-		Log.log("MidiTracker", "sendPortamentoOff() tick {} ", tick);
-	    sendControlChange(channel, 65, 0,  tick);
+        Log.log("MidiTracker", "sendPortamentoOff() tick {} ", tick);
+        sendControlChange(channel, 65, 0,  tick);
     }
 
 
 
-	////////////////
-	// INTERNALS: //
-	////////////////
+    ////////////////
+    // INTERNALS: //
+    ////////////////
 
-	/** Get the most significant 7 - not 8 - bits */
-	private int getMSB(int value) {
-		return value >>> 7;
-	}
+    /** Get the most significant 7 - not 8 - bits */
+    private int getMSB(int value) {
+        return value >>> 7;
+    }
 
-	/** Get the least significant 7 bits */
-	private int getLSB(int value) {
-		return value & 127; // 127 is binary 0111 1111
-	}
+    /** Get the least significant 7 bits */
+    private int getLSB(int value) {
+        return value & 127; // 127 is binary 0111 1111
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////
@@ -147,14 +145,14 @@ public class MidiTracker  {
     ////////////////////////////////////////////////////////////////////////////////////////////
 
     private void sendControlChange(int channel, int data1, int data2, long tick)  {
-	    event(channel, ShortMessage.CONTROL_CHANGE, data1, data2, tick);
-	}
+        event(channel, ShortMessage.CONTROL_CHANGE, data1, data2, tick);
+    }
 
     private void event(int channel, int type, int data1, int data2, long tick) {
         Except.run(()->
             sendMessage(
-	            new ShortMessage(type, channel, data1, data2),
-	            tick
+                new ShortMessage(type, channel, data1, data2),
+                tick
             )
         );
     }
